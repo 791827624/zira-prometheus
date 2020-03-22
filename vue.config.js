283742f40ee9acd1f49c2d2b0e3c880cda474f62
zzc <791827624@qq.com>
2020-03-23 02:46:28 +0800

    fix congig.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,15 @@
 module.exports = {
     // cli3 代理是从指定的target后面开始匹配的，不是任意位置；配置pathRewrite可以做替换
+    publicPath:'./',
     devServer: {
+        port:8081,
+        overlay: { //通过设置让浏览器 overlay 同时显示警告和错误
+            warnings: true,
+            errors: true
+        },
         proxy: {
-            '/': {
+            './': {
                 target: 'http://101.132.169.36:9090',
-
                 changeOrigin: true,
                 secure: false,
                 /*pathRewrite: { }*/
