e0f89b467d49675b8157813fdd9d49f04101efd8
zzc <791827624@qq.com>
2020-03-28 11:46:34 +0800

    fixed bug

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
       }
       ,
       mounted(){
-            if (localStorage.getItem("token") == undefined || localStorage.getItem("token" == null)){
+            if (localStorage.getItem("token" == undefined) || localStorage.getItem("token" == null)){
               router.push("/login")
 
             }else{
